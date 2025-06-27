2ce724a9a43422346328402c6a054fe9d7118979
James Puleo <james@jame.xyz>
2025-06-27 00:59:44 -0400

    Don't `V_swap` on bitfields

--- public/tier1/utlblockmemory.h
+++ public/tier1/utlblockmemory.h
@@ -139,8 +139,14 @@ void CUtlBlockMemory<T,I>::Swap( CUtlBlockMemory< T, I > &mem )
 {
 	V_swap( m_pMemory, mem.m_pMemory );
 	V_swap( m_nBlocks, mem.m_nBlocks );
-	V_swap( m_nIndexMask, mem.m_nIndexMask );
-	V_swap( m_nIndexShift, mem.m_nIndexShift );
+
+	int ourIndexMask = m_nIndexMask;
+	m_nIndexMask = mem.m_nIndexMask;
+	mem.m_nIndexMask = ourIndexMask;
+
+	int ourIndexShift = m_nIndexShift;
+	m_nIndexShift = mem.m_nIndexShift;
+	mem.m_nIndexShift = ourIndexShift;
 }
 
 
